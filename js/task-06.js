43432184ffa27fecfa115a752b70f55c72e76dd7
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-04-14 09:48:24 +0200

    task 6 update

--- js/task-06.js
+++ js/task-06.js
@@ -7,8 +7,10 @@ textInput.addEventListener('blur', checkLength);
 
 function checkLength() {
   if (textInput.value.length === maxLength) {
+    textInput.classList.remove('invalid');
     textInput.classList.add('valid');
   } else {
+    textInput.classList.remove('valid');
     textInput.classList.add('invalid');
   }
 }
